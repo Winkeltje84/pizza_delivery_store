514d52062fcfcb6410eaf43b7dfeb621a2f16432
Winkeltje84 <bwinkel@gmail.com>
2017-03-08 16:09:38 +0100

    first changes

---
+++ pizza_delivery.rb
@@ -0,0 +1,26 @@
+=begin
+
+Lab 1 / Pizza Delivery
+
+Pizza Delivery - SUMMARY ASSIGNMENT
+
+The first lab will be about classes and how they can store data.
+You will make a pizza delivery service.
+
+It will contain the following classes:
+Menu : All the possible options to order
+Pizza : A pizza
+Order : Contains one or more pizza's
+
+There will also be a main file for the program.
+
+=end
+
+done = false
+
+put "Hello welcome to Bas' pizza Winkel!"
+
+#below while-loop will run untill variable 'done' == true
+while not done
+  
+end
